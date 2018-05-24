b71a42244eab5d525eb46ce42ff69d12c77ed659
apekearo <apekearo@gmail.com>
2018-05-23 20:27:47 -0500

    added about page and content, as well as white background to all text

--- about.js
+++ about.js
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Knewave" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
     <link rel="stylesheet" type="text/css" href="style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>    <script src="function.js"></script>
@@ -15,7 +16,7 @@
 </head>
 <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="home.html">EMHU</a>
+                <a class="navbar-brand" href="#">EMHU</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
@@ -28,37 +29,32 @@
                     <a class="nav-item nav-link" href="tour.html">tour</a>
                     <a class="nav-item nav-link" href="zine.html">zine</a>
                     <a class="nav-item nav-link" href="faq.html">faq</a>
-                    <a class="nav-item nav-link" href="about.html">about</a>
-
                   </div>
                 </div>
               </nav>
               <br>
               <div class="header">
-    <h1 style="align-content: center" class="text-center jumbotron">Ethan Master Of The Hawiian Ukelele</h1>
-        </div>      <h2 class="text-center" style="background-color:white">TOUR</h2>
-
-        <h3 class="text-center filler" style="background-color:white"> Come Catch The Wave!! </h3>
-        <br>
-        <br>
-        <div class="container">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="lead panel-title text-center">"Make It Special" 2018 Full Tour Dates to be announced soon !!!!</h3>
-            
-          </div>
-          <div class="panel-body text-center">
-              June 29th -- Interdependence-Share Fest, NEW WINDSOR, ILLINOIS
-            </div>
-            <hr>
-            <div class="panel-body text-center">
-                July 10th -- WFMU taping
-              </div>
-              <hr>
-              <div class="panel-body text-center">
-                  July 11th -- Flowers for all Occasions , BROOKLYN
-                </div>
-        </div>
-      </div>
-      </body>
+    <h1 style="align-content: center" class="text-center jumbotron xs-5">Ethan Master Of The Hawiian Ukelele</h1>
+         </div>
+       <h2 class="text-center"style="background-color:white"> F.A.Q.</h2>
+       <br>
+       <hr>
+       <div class="container">
+          <h2 class="text-center p-3 mb-2 bg-light text-dark">Question:</h2>
+       <h3 class="text-center p-3 mb-2 bg-light text-dark">Are you a master of anything else?</h3>
+       <h2 class="text-center p-3 mb-2 bg-light text-dark">Answer:</h2>
+       <h3 class="text-center p-3 mb-2 bg-light text-dark">Also a master of perfect posture!</h3>
+       <hr>
+       <h2 class="text-center p-3 mb-2 bg-light text-dark">Question:</h2>
+       <h3 class="text-center p-3 mb-2 bg-light text-dark">Why are you called Ethan Master of the Hawaiian Ukulele if you don't play ukulele?
+        </h3>
+       <h2 class="text-center p-3 mb-2 bg-light text-dark">Answer:</h2>
+       <h3 class="text-center p-3 mb-2 bg-light text-dark">That is a great point. I guess you are what you are!</h3>
+       <hr>
+       <h2 class="text-center p-3 mb-2 bg-light text-dark">Question:</h2>
+       <h3 class="text-center p-3 mb-2 bg-light text-dark">What do you dream about at night?</h3>
+       <h2 class="text-center p-3 mb-2 bg-light text-dark">Answer:</h2>
+       <h3 class="text-center p-3 mb-2 bg-light text-dark" style="word-wrap:normal">Sometimes I have server nightmares, where I forget peoples order's and am in my underwear, or I think its Sunday but its really Saturday, and that means I have work again tomorrow, jesus.  I also have a reoccurring dream where I inhabit a space like an attic or a country pagoda where there are a bunch of special objects, and I think this may represent my desire for self-actualization.</h3>
+       <hr></div>
+</body>
 </html>
